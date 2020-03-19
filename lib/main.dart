acb94c13a7eddd0f407a8eadb4e2e01585e3f9e4
Hassan78 <hassan.ali@g.austincc.edu>
2020-03-18 21:49:17 -0500

    modifying portfolio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/screens/about.dart';
 
 
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: About(),
       
     );
